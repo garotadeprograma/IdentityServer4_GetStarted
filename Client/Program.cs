4accfc5e54435ef6954a71c94dcf7898506f1108
anna.silva <anna.silva@ituan.com.br>
2022-04-30 14:29:23 -0300

    Configuração da classe Client para acessar as informações do identity server

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using IdentityModel.Client;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Client
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task  Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //descobrir os endpoints através dos metadados
+            var client = new HttpClient();
+            var discovery = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+
+            if (discovery.IsError)
+            {
+                Console.WriteLine(discovery.Error);
+                return;
+            }
+
+            //request token
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync
+                                (
+                                    new ClientCredentialsTokenRequest
+                                    {
+                                        Address = discovery.TokenEndpoint,
+                                        ClientId = "client",
+                                        ClientSecret = "secret",
+                                        Scope = "api1"
+                                    }
+                                );
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+
+            Console.ReadLine();
         }
     }
 }
